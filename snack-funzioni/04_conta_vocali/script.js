d505bc131f177706e6238443bda14fb9b87f7781
DavideCotigliani <davide.cotigliani@gmail.com>
2025-03-24 15:53:24 +0100

    [UPD] called the function and defined the function in ex 4

--- snack-funzioni/04_conta_vocali/script.js
+++ snack-funzioni/04_conta_vocali/script.js
@@ -5,13 +5,18 @@ const word = 'javascript';
 
 // Dichiara la funzione qui.
 function countVowels (string){
-
+    const vowels = "aeiou"
+    let count = 0;
+    for(const char of string){
+        if (vowels.includes(char)){
+        count ++
+        }
+}
+return count
 }
 
 // Invoca la funzione qui e stampa il risultato in console
 let numbersVowels = countVowels(word);
 console.log(numbersVowels);
-
-
 
 //Risultato atteso se si passa 'javascript': 3 (a, a, i)
